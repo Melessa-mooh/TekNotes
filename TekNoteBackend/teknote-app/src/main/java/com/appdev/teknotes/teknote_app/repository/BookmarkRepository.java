7d3df55a6494c885f399807dd39b7b020a33bdd2
Ma. Melessa V. Cabasag <159626113+Melessa-mooh@users.noreply.github.com>
2025-11-26 19:04:29 +0800

    Delete TekNoteBackend/teknote-app directory

--- TekNoteBackend/teknote-app/src/main/java/com/appdev/teknotes/teknote_app/repository/BookmarkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.appdev.teknotes.teknote_app.repository;
-
-import com.appdev.teknotes.teknote_app.entity.Bookmark;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    boolean existsByUser_IdAndResource_Id(Long userId, Long resourceId);
-}
--- TekNoteBackend/teknote-app/src/main/java/com/appdev/teknotes/teknote_app/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.appdev.teknotes.teknote_app.repository;
-
-import com.appdev.teknotes.teknote_app.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    Course findByCourseCode(String courseCode);
-}
