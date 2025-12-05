b568646cbd303d76c6417733cb787a4c8a590a66
kirito <benoliraoclyde71@gmail.com>
2025-12-05 12:21:04 +0800

    Connect Reviews backend to frontend

---
+++ TekNotes-Backend/bcd/src/main/java/com/appdevg4/bcd/teknotes/dto/ReviewRequest.java
@@ -0,0 +1,11 @@
+package com.appdevg4.bcd.teknotes.dto;
+
+import lombok.Data;
+
+@Data
+public class ReviewRequest {
+    private Integer userId;
+    private Integer resourceId;
+    private Integer rating;
+    private String comment;
+}
